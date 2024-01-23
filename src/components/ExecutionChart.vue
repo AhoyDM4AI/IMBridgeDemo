575df17befc262b43682fa08eaaf9e1a8bc6f7e4
watch2bear <ypsy08@163.com>
2024-01-23 16:56:46 +0800

    feat: refine echarts and draw markline

--- src/components/ExecutionChart.vue
+++ src/components/ExecutionChart.vue
@@ -22,6 +22,14 @@ const init = () => {
 
 const reDraw = (chartData) => {
   let option = {
+    dataZoom: [{
+      show: true,
+      type:"inside",
+      maxValueSpan: 40,
+      minValueSpan: 20,
+      startValue: 0,
+      endValue: 40,
+    }],
     xAxis: {
       name: "Iteration Number",
       type: "category",
@@ -40,6 +48,26 @@ const reDraw = (chartData) => {
       top: "top",
       //left: 160
     },
+    tooltip: {
+      // 当trigger : axis 鼠标移入标线不会有悬浮框出现 params的值为一个数组，只会悬浮series
+      // 当trigger : item 鼠标移入标线有悬浮框出现 params的值为一个对象，会区别鼠标移入的是series还是markLine
+      // 使用axis 注释掉formatter 自己写也行 ；使用item放开formatter
+      show: true,
+      trigger: "item",//axis item
+      axisPointer: {
+          type: "cross",
+          label: {
+              backgroundColor: "#6a7985",
+          },
+      },
+      formatter: (params) => {
+          if(params.componentType === "series"){
+              return "iter: " + params.name + '<br>' + params.marker+' '+params.data;
+          }else if(params.componentType === "markLine"){
+              return params.data.label.formatter + ': ' + params.value ;
+          }
+      }
+    },
     series: [
       {
         name: "OceanBase",
@@ -52,7 +80,28 @@ const reDraw = (chartData) => {
         type: "bar",
       },
     ],
+    textStyle: {
+      fontSize: 18,
+    }
   };
+  let markLine_option = {
+    symbol: ['none', 'none'],
+    data: [{
+      type: "max",
+      name: "Desirable Batch Size",
+      label: {
+        show: true,
+        position: 'end',
+        formatter: 'Desirable'
+      },
+      lineStyle: {
+        color: "black",
+      },
+    }],
+  }
+  if (props.chart_id == "batch") 
+    option.series[1].markLine = markLine_option;
+
   option && myChart.setOption(option);
 };
 onMounted(() => {
